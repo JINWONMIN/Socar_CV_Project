7be847707295ce2080ee5c8f4d96053da2db74e3
민진원 <94345086+JINWONMIN@users.noreply.github.com>
2022-05-10 14:07:14 +0900

    Add files via upload

---
+++ deeplabv3/model/deeplabv3.py
@@ -0,0 +1,17 @@
+"""DeepLabv3 Model download and change the head for your prediction"""
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead
+from torchvision import models
+
+def createDeepLabv3(outputchannels=1):
+    """DeepLabv3 class with custom head
+    :arg
+        outputchannels (int, optional): The number of output channels in dataset masks. Defaults to 1.
+    :return
+        model: Returns the DeepLabv3 model with the ResNet101 backbone.
+    """
+    model = models.segmentation.deeplabv3_resnet101(pretrained=True,
+                                                    progress=True)
+    model.classifier = DeepLabHead(2048, outputchannels)
+    # Set the model in training mode
+    model.train()
+    return model
